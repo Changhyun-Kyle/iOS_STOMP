44fdcf82c729c8a916c205eeaef5a677b482d7d7
Changhyun-Kyle <changhyun.kyle@gmail.com>
2024-11-19 17:49:29 +0900

    config: 프로젝트 생성

---
+++ iOS_STOMP/iOS_STOMP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_STOMP
+//
+//  Created by 아티젠스페이스 on 11/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
